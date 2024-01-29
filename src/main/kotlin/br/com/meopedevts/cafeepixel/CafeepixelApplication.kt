01609aa704e60325eebdbc7a2609ffa6342688c4
meopedevts <marcelo.meope@gmail.com>
2024-01-28 23:50:29 -0300

    Start Project

---
+++ src/main/kotlin/br/com/meopedevts/cafeepixel/CafeepixelApplication.kt
@@ -0,0 +1,11 @@
+package br.com.meopedevts.cafeepixel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeepixelApplication
+
+fun main(args: Array<String>) {
+	runApplication<CafeepixelApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/meopedevts/cafeepixel/CafeepixelApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.meopedevts.cafeepixel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CafeepixelApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
